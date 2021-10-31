567f0d3405ffd0b797c906a9d1d3cfc23e70be3c
Lakeesha Ramanayake <65113676+LakeeshaRCL@users.noreply.github.com>
2021-10-31 20:14:39 +0530

    availableBusListPage added

---
+++ lib/availableBusList.dart
@@ -0,0 +1,28 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class AvailableBusListPage extends StatefulWidget {
+  const AvailableBusListPage({ Key? key }) : super(key: key);
+
+  @override
+  _AvailableBusListPageState createState() => _AvailableBusListPageState();
+}
+
+class _AvailableBusListPageState extends State<AvailableBusListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Travel Partner"),
+      ),
+      body: SafeArea(
+        child:Column(
+          children: <Widget>[
+            Text("Next available busses for your destination"),
+          ],
+        ),
+      ),
+    );
+  }
+}
