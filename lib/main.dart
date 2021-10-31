c8e5fdcf71a5a364be86cd8c44c6cd46d5b3c10d
HirukaRanasinghe <62961312+HirukaRanasinghe@users.noreply.github.com>
2021-10-31 09:22:22 +0530

    Add Validators

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,12 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+
+  bool isValidEmail(String emailAddress) {
+    return RegExp(
+        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$')
+        .hasMatch(emailAddress);
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -51,22 +57,39 @@ class _LoginPageState extends State<LoginPage> {
             SizedBox(
               height: 150,
             ),
-            TextField(
+            TextFormField(
               decoration: InputDecoration(
                   labelText: 'Email',
                   labelStyle: TextStyle(fontSize: 20),
-                  filled: true),
+                  filled: true
+              ),
+              validator: (value){
+                if (value == null || value.isEmpty  || !isValidEmail(value)){
+                  return 'Please enter valid Email';
+                }
+                else{
+                  return null;
+                }
+              },
             ),
             SizedBox(
               height: 20,
             ),
-            TextField(
+            TextFormField(
               decoration: InputDecoration(
                 labelText: 'Password',
                 labelStyle: TextStyle(fontSize: 20),
                 filled: true,
               ),
               obscureText: true,
+              validator: (value){
+                if (value == null || value.isEmpty){
+                  return 'Please enter Password';
+                }
+                else{
+                  return null;
+                }
+              },
             ),
             SizedBox(
               height: 20,
